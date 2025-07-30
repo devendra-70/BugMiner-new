2ef557d1f18ccfd7f837e54c1c106caeb2d73c24
devendra-70 <devendramahajan70@gmail.com>
2025-07-30 20:09:35 +0530

    added docker backends

---
+++ langs-service/src/main/java/com/BugMiner/langs_service/LangsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.BugMiner.langs_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LangsServiceApplication.class, args);
+	}
+
+}
---
+++ langs-service/src/test/java/com/BugMiner/langs_service/LangsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BugMiner.langs_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
