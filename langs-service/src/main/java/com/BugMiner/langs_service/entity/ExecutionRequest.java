0d92f8e376af857f0db98768f84b6128ca342c64
devendra-70 <devendramahajan70@gmail.com>
2025-07-30 20:40:09 +0530

    added models in langs service

---
+++ langs-service/src/main/java/com/BugMiner/langs_service/entity/ExecutionRequest.java
@@ -0,0 +1,7 @@
+package com.BugMiner.langs_service.entity;
+
+public class ExecutionRequest {
+    private String code;
+    private String language; // "java", "cpp", "python"
+    private String stdin;
+}
---
+++ langs-service/src/main/java/com/BugMiner/langs_service/entity/ExecutionResult.java
@@ -0,0 +1,8 @@
+package com.BugMiner.langs_service.entity;
+
+public class ExecutionResult {
+    private boolean success;
+    private String output;
+    private String error;
+    private int exitCode;
+}
