39e504e6856d69d2e97416645928c22fcd9fc2b3
devendra-70 <devendramahajan70@gmail.com>
2025-07-30 18:56:01 +0530

    initial commit

---
+++ codex/src/main/java/com/BugMiner/codex/CodexApplication.java
@@ -0,0 +1,13 @@
+package com.BugMiner.codex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodexApplication.class, args);
+	}
+
+}
